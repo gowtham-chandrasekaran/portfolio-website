33eae643ffd14e43c84bf814eac863b321f56c13
Gowtham Chandrasekaran <gowtham.chandrasekaran@sjsu.edu>
2023-02-04 02:09:15 -0800

    about content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,20 +14,17 @@ const About = () => {
         </div>
 
         <p className="text-xl mt-20">
-          I am a highly motivated software developer with experience in full
-          stack development. I have a passion for building intuitive,
-          user-friendly applications and solving complex problems through code.
-          With 2 years of work experience, I have honed my skills in front-end
-          and back-end technologies and have a track record of delivering
-          high-quality, reliable software solutions.
+          I'm currently pursuing MS in Computer Science at San Jose State
+          University. I will be graduating in May 2023. With 2 years of work
+          experience, I have honed my skills in front-end and back-end
+          technologies and have a track record of delivering high-quality,
+          reliable software solutions.
         </p>
         <br />
         <p className="text-xl">
-          I am excited to announce that I will be available for new
-          opportunities starting in May 2023. I am eager to join a company that
-          values collaboration, innovation, and growth, and I believe my skills
-          and passion for software development make me a strong fit for a
-          variety of roles.
+          I am eager to join a company that values collaboration, innovation,
+          and growth, and I believe my skills and passion for software
+          development make me a strong fit for a variety of roles.
         </p>
       </div>
     </div>
