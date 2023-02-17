987f6f98c1e6fa604433a73d7f72fbcad111a1c1
Gowtham Chandrasekaran <gowtham.chandrasekaran@sjsu.edu>
2023-02-17 00:17:09 -0800

    upp

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -113,14 +113,7 @@ const Technologies = () => {
 
         <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
           {techs.map(({ id, src, title, style }) => (
-            <div
-              key={id}
-              className={
-                "shadow-md betterhover:scale-105 duration-500 py-2 rounded-lg" +
-                " " +
-                style
-              }
-            >
+            <div key={id} className={"shadow-md py-2 rounded-lg" + " " + style}>
               <img src={src} alt="" className="w-20 mx-auto" />
               <p className="mt-4">{title}</p>
             </div>
